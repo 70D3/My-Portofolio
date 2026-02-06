069fb8a031d770c60953e997c1bc528be2579848
Mr.Jode <yonathandewangga321@gmail.com>
2026-02-06 21:11:40 +0700

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero ipsam,
+        dolor, voluptatibus repellat quos beatae vero minima cupiditate aut
+        maxime at hic ipsum deserunt dolore magnam consectetur labore facere
+        repudiandae, quaerat eius. Sint ullam, praesentium possimus eaque odio,
+        pariatur maiores eveniet placeat, quis dolorem quisquam blanditiis
+        aspernatur eius modi ipsa deleniti debitis sed perspiciatis at minima
+        similique? Quisquam sit rerum quidem officia veritatis perferendis odio
+        sed nihil minima beatae porro similique eum placeat, assumenda provident
+        laboriosam? Rem qui molestias esse facere odio optio nisi, ab similique
+        vitae dicta, sunt adipisci reiciendis ullam sed repudiandae delectus
+        veritatis asperiores corporis ad vel rerum et ex? Aliquid dolore impedit
+        excepturi ad laudantium accusantium enim tempore, illo labore suscipit
+        vitae quos optio incidunt expedita consequuntur perferendis ea tempora.
+        Cum doloribus nulla natus sunt adipisci obcaecati quisquam, nostrum
+        provident corrupti, dignissimos, magni sint voluptatem qui similique
+        repellendus facilis ipsum animi iusto id aut. Laborum maiores, iure
+        doloribus possimus, debitis molestiae sint magnam suscipit, in
+        dignissimos dolore tempora iste quas quod reprehenderit? Nihil
+        blanditiis aliquid ut libero quidem impedit incidunt temporibus culpa
+        id? Voluptas, vitae sapiente magni, necessitatibus a similique deserunt
+        rem tempore doloribus suscipit saepe et veniam ratione consectetur eius
+        nemo quidem eaque. Dignissimos, quidem.
+      </h1>
+    </>
+  );
+}
+
+export default App;
